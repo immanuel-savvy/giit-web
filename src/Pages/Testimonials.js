d7b0828add9d81d37382b4adb5728524ee86e921
immanuel-savvy <immanuelsavvy@gmail.com>
2023-06-09 16:52:24 +0100

    1686325944178838087

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -9,6 +9,7 @@ import Listempty from "../Components/list_empty";
 import Loadindicator from "../Components/loadindicator";
 import Add_student_review from "../Sections/dashboard/add_review_form";
 import { organisation_name } from "../Constants/constants";
+import Video_review from "../Components/video_review";
 
 class Testimonials extends React.Component {
   constructor(props) {
@@ -31,15 +32,16 @@ class Testimonials extends React.Component {
       limit: page_size,
     });
 
-    console.log(reviews);
-
     this.setState({ reviews });
+
+    let video_reviews = await post_request("video_reviews");
+    this.setState({ video_reviews });
   };
 
   toggle_form = () => this.setState({ show_form: !this.state.show_form });
 
   render() {
-    let { reviews, show_form, review_submitted } = this.state;
+    let { reviews, show_form, video_reviews, review_submitted } = this.state;
 
     return (
       <div id="main-wrapper">
@@ -53,7 +55,45 @@ class Testimonials extends React.Component {
 
         <div style={{ backgroundColor: "#f7f8f9", paddingTop: 20 }}>
           <div className="container">
+            <div class="row justify-content-center my-3">
+              <div class="col-lg-7 col-md-8">
+                <div class="sec-heading center">
+                  <h2>
+                    Our Video <span class="theme-cl">Reviews</span>
+                  </h2>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+
             <div className="row justify-content-center">
+              {video_reviews ? (
+                video_reviews.length ? (
+                  video_reviews.map((r) => (
+                    <Video_review
+                      review={r}
+                      class_name="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-2"
+                      key={r._id}
+                    />
+                  ))
+                ) : null
+              ) : (
+                <Loadindicator />
+              )}
+            </div>
+
+            <div class="row justify-content-center my-3 mt-5">
+              <div class="col-lg-7 col-md-8">
+                <div class="sec-heading center">
+                  <h2>
+                    <span class="theme-cl">Reviews</span>
+                  </h2>
+                  <p>More on what our happy alumnis has to say</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-5 row justify-content-center">
               {reviews ? (
                 reviews.length ? (
                   reviews.map((review) => (
